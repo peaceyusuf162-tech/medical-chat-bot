9fbda1e3e3c46d57e69cb208e591dd9ba7f8d3ed
peaceyusuf162-tech <peaceyusuf162@gmail.com>
2026-06-01 23:12:45 +0100

    requirement added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="peace Agbu",
+    author_email="peaceyusuf162@gmail.com",
+    packages=find_packages(), 
+    install_requires=[]
+)
